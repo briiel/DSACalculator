4241104daeed8f2d69daa08c33890cf8d7030a30
briiel <144537019+briiel@users.noreply.github.com>
2023-10-09 21:08:20 +0800

    Add files via upload

---
+++ calculator/src/calculator/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author Admin
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static double Add(double num1, double num2){
+        return num1 + num2;
+    }
+    
+}
